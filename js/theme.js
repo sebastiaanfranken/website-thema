7eb92783ec35706967de4bd27944cbddfbde7412
Sebastiaan Franken <sebastiaan@sebastiaanfranken.nl>
2015-10-23 20:16:29 +0200

    Nieuwe versie

---
+++ js/theme.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", function() {
+
+	var header = document.querySelector("#site-header"),
+		headerHeight = window.getComputedStyle(header).height.replace("px", ""),
+		body = document.querySelector("body"),
+		previousTop = 0;
+
+	if(body.classList.contains("page-home")) {
+		if(window.scrollY > headerHeight && !header.classList.contains("detached")) {
+			header.classList.add("detached");
+		}
+
+		window.addEventListener("scroll", function(event) {
+
+			var currentTop = window.scrollY;
+
+			if(currentTop < previousTop && currentTop < headerHeight && header.classList.contains("detached")) {
+				header.classList.remove("detached");
+			}
+
+			if(currentTop > headerHeight && !header.classList.contains("detached")) {
+				header.classList.add("detached");
+			}
+
+			previousTop = currentTop;
+
+		}, false);
+	}
+	else {
+		header.classList.add("detached");
+	}
+
+}, false);
